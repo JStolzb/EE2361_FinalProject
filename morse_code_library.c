a72307c00272cbf599052acf51edf8c2d6b7dbc3
John Schray <stolz050@umn.edu>
2022-04-21 13:54:23 -0500

    Morse code lib bug fixes

--- morse_code_library.c
+++ morse_code_library.c
@@ -1,112 +1,125 @@
 #include "xc.h"
-
-// CW1: FLASH CONFIGURATION WORD 1 (see PIC24 Family Reference Manual 24.1)
-#pragma config ICS = PGx1          // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
-#pragma config FWDTEN = OFF        // Watchdog Timer Enable (Watchdog Timer is disabled)
-#pragma config GWRP = OFF          // General Code Segment Write Protect (Writes to program memory are allowed)
-#pragma config GCP = OFF           // General Code Segment Code Protect (Code protection is disabled)
-#pragma config JTAGEN = OFF        // JTAG Port Enable (JTAG port is disabled)
-
-
-// CW2: FLASH CONFIGURATION WORD 2 (see PIC24 Family Reference Manual 24.1)
-#pragma config I2C1SEL = PRI       // I2C1 Pin Location Select (Use default SCL1/SDA1 pins)
-#pragma config IOL1WAY = OFF       // IOLOCK Protection (IOLOCK may be changed via unlocking seq)
-#pragma config OSCIOFNC = ON       // Primary Oscillator I/O Function (CLKO/RC15 functions as I/O pin)
-#pragma config FCKSM = CSECME      // Clock Switching and Monitor (Clock switching is enabled, 
-                                       // Fail-Safe Clock Monitor is enabled)
-#pragma config FNOSC = FRCPLL      // Oscillator Select (Fast RC Oscillator with PLL module (FRCPLL))
-
-
-void setup(void) {
-        CLKDIVbits.RCDIV = 0;  //Set RCDIV=1:1 (default 2:1) 32MHz or FCY/2=16M
-        AD1PCFG = 0xffff;
-}
+#include "button_library.h"
 
 char morseCodeLib(const char s[]) {
     // Given an binary encoded morse character, this function will
     // return the associated character
-    // This library handles A-Z and 0-9
-    switch(s) {
-        case '01':
+    // This library handles A-Z, 0-9, and .,?
+        if (s == "01"){
             return 'A';
-        case '1000':
+        }
+        else if (s == "1000"){
             return 'B';
-        case '1010':
+        }
+        else if (s == "1010"){
             return 'C';
-        case '100':
+        }
+        else if (s == "100"){
             return 'D';
-        case '0':
+        }
+        else if (s == "0"){
             return 'E';
-        case '0010':
+        }
+        else if (s == "0010"){
             return 'F';
-        case '110':
+        }
+        else if (s == "110"){
             return 'G';
-        case '0000':
+        }
+        else if (s == "0000"){
             return 'H';
-        case '00':
+        }
+        else if (s == "00"){
             return 'I';
-        case '0111':
+        }
+        else if (s == "0111"){
             return 'J';
-        case '101':
+        }
+        else if (s == "101"){
             return 'K';
-        case '0100':
+        }
+        else if (s == "0100"){
             return 'L';
-        case '11':
+        }
+        else if (s == "11"){
             return 'M';
-        case '10':
+        }
+        else if (s == "10"){
             return 'N';
-        case '111':
+        }
+        else if (s == "111"){
             return 'O';
-        case '0110':
+        }
+        else if (s == "0110"){
             return 'P';
-        case '1101':
+        }
+        else if (s == "1101"){
             return 'Q';
-        case '010':
+        }
+        else if (s == "010"){
             return 'R';
-        case '000':
+        }
+        else if (s == "000"){
             return 'S';
-        case '1':
+        }
+        else if (s == "1"){
             return 'T';
-        case '001':
+        }
+        else if (s == "001"){
             return 'U';
-        case '0001':
+        }
+        else if (s == "0001"){
             return 'V';
-        case '011':
+        }
+        else if (s == "011"){
             return 'W';
-        case '1001':
+        }
+        else if (s == "1001"){
             return 'X';
-        case '1011':
+        }
+        else if (s == "1011"){
             return 'Y';
-        case '1100':
+        }
+        else if (s == "1100"){
             return 'Z';
-        case '0111':
+        }
+        else if (s == "0111"){
             return '1';
-        case '00111':
+        }
+        else if (s == "00111"){
             return '2';
-        case '00011':
+        }
+        else if (s == "00011"){
             return '3';
-        case '00001':
+        }
+        else if (s == "00001"){
             return '4';
-        case '00000':
+        }
+        else if (s == "00000"){
             return '5';
-        case '10000':
+        }
+        else if (s == "10000"){
             return '6';
-        case '11000':
+        }
+        else if (s == "11000"){
             return '7';
-        case '11100':
+        }
+        else if (s == "11100"){
             return '8';
-        case '11110':
+        }
+        else if (s == "11110"){
             return '9';
-        case '11111':
+        }
+        else if (s == "11111"){
             return '0';
-    }
-}
-
-
-int main(void) {
-    setup();
-    while(1) {
-        
-    }
-    return 0;
+        }
+        else if (s == "010101"){
+            return '.';
+        }
+        else if (s == "110011"){
+            return ',';
+        }
+        else if (s == "001100"){
+            return '?';
+        }
 }
