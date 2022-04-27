19e202c0c1ebcb4a90e086a68fcb3c95d574f8d1
JikSmoth <103676581+JikSmoth@users.noreply.github.com>
2022-04-27 13:29:26 -0500

    Add files via upload

---
+++ MorseCodeLib.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   MorseCodeLib.h
+ * Author: jacks
+ *
+ * Created on April 22, 2022, 2:09 PM
+ */
+
+#ifndef MORSECODELIB_H
+#define	MORSECODELIB_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+char morseCodeLib(char* s);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MORSECODELIB_H */
+
