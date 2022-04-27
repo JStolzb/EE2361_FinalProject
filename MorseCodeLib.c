4378b2408fee6cb0ec8cdca9fb292d0843c36aca
John Schray <stolz050@umn.edu>
2022-04-27 15:00:38 -0500

    strcmp string compare

--- MorseCodeLib.c
+++ MorseCodeLib.c
@@ -13,121 +13,121 @@ char morseCodeLib(void) {
     // return the associated character
     // This library handles A-Z, 0-9, and .,?
     
-        if (letter == "01NNNN"){
+        if (!strcmp(letter, "01NNNN")){
             return 'A';
         }
-        else if (letter == "1000NN"){
+        else if (!strcmp(letter, "1000NN")){
             return 'B';
         }
-        else if (letter == "1010NN"){
+        else if (!strcmp(letter, "1010NN")){
             return 'C';
         }
-        else if (letter == "100NNN"){
+        else if (!strcmp(letter, "100NNN")){
             return 'D';
         }
-        else if (letter == "0NNNNN"){
+        else if (!strcmp(letter, "0NNNNN")){
             return 'E';
         }
-        else if (letter == "0010NN"){
+        else if (!strcmp(letter, "0010NN")){
             return 'F';
         }
-        else if (letter == "110NNN"){
+        else if (!strcmp(letter, "110NNN")){
             return 'G';
         }
-        else if (letter == "0000NN"){
+        else if (!strcmp(letter, "0000NN")){
             return 'H';
         }
-        else if (letter == "00NNNN"){
+        else if (!strcmp(letter, "00NNNN")){
             return 'I';
         }
-        else if (letter == "0111NN"){
+        else if (!strcmp(letter, "0111NN")){
             return 'J';
         }
-        else if (letter == "101NNN"){
+        else if (!strcmp(letter, "101NNN")){
             return 'K';
         }
-        else if (letter == "0100NN"){
+        else if (!strcmp(letter, "0100NN")){
             return 'L';
         }
-        else if (letter == "11NNNN"){
+        else if (!strcmp(letter, "11NNNN")){
             return 'M';
         }
-        else if (letter == "10NNNN"){
+        else if (!strcmp(letter, "10NNNN")){
             return 'N';
         }
-        else if (letter == "111NNN"){
+        else if (!strcmp(letter, "111NNN")){
             return 'O';
         }
-        else if (letter == "0110NN"){
+        else if (!strcmp(letter, "0110NN")){
             return 'P';
         }
-        else if (letter == "1101NN"){
+        else if (!strcmp(letter, "1101NN")){
             return 'Q';
         }
-        else if (letter == "010NNN"){
+        else if (!strcmp(letter, "010NNN")){
             return 'R';
         }
-        else if (letter == "000NNN"){
+        else if (!strcmp(letter, "000NNN")){
             return 'S';
         }
-        else if (letter == "1NNNNN"){
+        else if (!strcmp(letter, "1NNNNN")){
             return 'T';
         }
-        else if (letter == "001NNN"){
+        else if (!strcmp(letter, "001NNN")){
             return 'U';
         }
-        else if (letter == "0001NN"){
+        else if (!strcmp(letter, "0001NN")){
             return 'V';
         }
-        else if (letter == "011NNN"){
+        else if (!strcmp(letter, "011NNN")){
             return 'W';
         }
-        else if (letter == "1001NN"){
+        else if (!strcmp(letter, "1001NN")){
             return 'X';
         }
-        else if (letter == "1011NN"){
+        else if (!strcmp(letter, "1011NN")){
             return 'Y';
         }
-        else if (letter == "1100NN"){
+        else if (!strcmp(letter, "1100NN")){
             return 'Z';
         }
-        else if (letter == "0111NN"){
+        else if (!strcmp(letter, "0111NN")){
             return '1';
         }
-        else if (letter == "00111N"){
+        else if (!strcmp(letter, "00111N")){
             return '2';
         }
-        else if (letter == "00011N"){
+        else if (!strcmp(letter, "00011N")){
             return '3';
         }
-        else if (letter == "00001N"){
+        else if (!strcmp(letter, "00001N")){
             return '4';
         }
-        else if (letter == "00000N"){
+        else if (!strcmp(letter, "00000N")){
             return '5';
         }
-        else if (letter == "10000N"){
+        else if (!strcmp(letter, "10000N")){
             return '6';
         }
-        else if (letter == "11000N"){
+        else if (!strcmp(letter, "11000N")){
             return '7';
         }
-        else if (letter == "11100N"){
+        else if (!strcmp(letter, "11100N")){
             return '8';
         }
-        else if (letter == "11110N"){
+        else if (!strcmp(letter, "11110N")){
             return '9';
         }
-        else if (letter == "11111N"){
+        else if (!strcmp(letter, "11111N")){
             return '0';
         }
-        else if (letter == "010101"){
+        else if (!strcmp(letter, "010101")){
             return '.';
         }
-        else if (letter == "110011"){
+        else if (!strcmp(letter, "110011")){
             return ',';
         }
-        else if (letter == "001100"){
+        else if (!strcmp(letter, "001100")){
             return '?';
         }
         else{
